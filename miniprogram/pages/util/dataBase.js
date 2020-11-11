54c239a8b8f9617416c5c26e0af8f25cc61e1d29
jeans-pc <jianrenjian@live.com>
2020-11-11 23:53:38 +0800

    user表增加_id编号
    user表增加_id编号
    还在做赛季显示
    准备重做单场逻辑

--- miniprogram/pages/util/dataBase.js
+++ miniprogram/pages/util/dataBase.js
@@ -13,6 +13,30 @@ function onQuery(databaseName, selectName, selectShuxing, selectName1, selectShu
   }).get()
 }
 
+function player_id(){
+  var index = 0
+  onQuery("playid","_id",play_id).then(res=>{
+    if(res.data.length==0){
+      this.test = that.db.collection('playid')
+        this.test.add({
+          // data 字段表示需新增的 JSON 数据
+          data: {
+            _id:index
+          },
+          //  数据插入成功，调用该函数
+          success: function (res) {
+            wx.showToast({
+              title: '新增用户成功',
+            })
+            //增加新增用户日志
+            var log = "[新增用户]nickName:" + that.data.nickName
+            dateBase.insertLog(log)
+          }
+        })
+    }
+  })
+}
+
 function onRemove(databaseName, selectId) {
   // console.log("语句",db.collection(databaseName).doc(selectId).remove())
   db.collection(databaseName).doc(selectId).remove()
--- miniprogram/pages/xiaobaipiUser/xiaobaipiUser.js
+++ miniprogram/pages/xiaobaipiUser/xiaobaipiUser.js
@@ -13,6 +13,7 @@ Page({
     avatarUrl: '../index/user-unlogin.png',
     name: "",
     nickName: "点击头像登陆",
+    player_id: 0,
     openid: "",
     userInfo: {},
     logged: false,
@@ -25,7 +26,6 @@ Page({
    */
   onLoad: function (options) {
     openid: getOpenid.getOpenid()
-
     if (!wx.cloud) {
       wx.redirectTo({
         url: '../chooseLib/chooseLib',
@@ -113,9 +113,12 @@ Page({
     })
   },
 
+
   // 单击“插入数据”按钮调用该函数
   addUser: function () {
     var that = this
+    var player_id = that.data.player_id
+    console.log("playid等于", player_id)
     // if (that.data.name == "") {
     //   wx.showModal({
     //     title: '错误',
@@ -125,38 +128,58 @@ Page({
     //   return
     // }
     try {
-      console.log("openid" + app.globalData.openid)
-      dataBase.onQuery("user", "_openid", app.globalData.openid).then(res => {
-        console.log(res.data.length)
+      dataBase.onQuery("user", "_id", player_id).then(res => {
+        console.log("addUser长度", res.data.length)
         if (res.data.length != 0) {
           wx.showModal({
             title: '错误',
             content: "用户已存在",
             showCancel: false
           })
+          this.setData({
+            player_id: player_id + 1
+          })
           return
         }
-        this.db = wx.cloud.database()
-        this.test = that.db.collection('user')
-        this.test.add({
-          // data 字段表示需新增的 JSON 数据
-          data: {
-            // name: that.data.name,
-            nickName: that.data.nickName,
-            avatarUrl: that.data.avatarUrl,
-          },
-          //  数据插入成功，调用该函数
-          success: function (res) {
-            wx.showToast({
-              title: '新增用户成功',
+        dataBase.onQuery("user", "_openid", app.globalData.openid).then(res1 => {
+          console.log("openid长度",res1.data.length)
+          if (res1.data.length != 0) {
+            wx.showModal({
+              title: '错误',
+              content: "用户已存在",
+              showCancel: false
             })
-            //增加新增用户日志
-            var log = "[新增用户]nickName:" + that.data.nickName
-            dateBase.insertLog(log)
+            this.setData({
+              player_id: player_id + 1
+            })
+            return
+          }else{
+            this.db = wx.cloud.database()
+            this.test1 = that.db.collection('user')
+            this.test1.add({
+              // data 字段表示需新增的 JSON 数据
+              data: {
+                _id: player_id,
+                nickName: that.data.nickName,
+                avatarUrl: that.data.avatarUrl,
+                player_id: player_id,
+              },
+              //  数据插入成功，调用该函数
+              success: function (res) {
+                wx.showToast({
+                  title: '新增用户成功',
+                })
+                //增加新增用户日志
+                var log = "[新增用户]nickName:" + that.data.nickName
+                dateBase.insertLog(log)
+              }
+            })
+            this.setData({
+              player_id : 0,
+            })
           }
         })
       })
-
     } catch (e) {
       wx.showModal({
         title: '错误',
